d39b1e83e8a342f76e17c2b0c8cf9a8f0529c5e9
Chathuranga97lj <chathuit97@gmail.com>
2022-02-28 08:55:43 +0530

    Change index.js

--- public/index.js
+++ public/index.js
@@ -3,6 +3,68 @@ const app = firebase.app()
 const cred = app._delegate._options;
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged,signOut } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 
-const _app = initializeApp(cred)
+export const _app = initializeApp(cred)  //export
 //console.log(_app); 
+
+const auth = getAuth()
+
+const login = document.getElementById('login-form');
+const signup = document.getElementById('signup-form');
+
+// #######################################################
+if (login){
+    login.onsubmit = e => {
+        e.preventDefault()
+        document.getElementById('signin-status').innerText = "";
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+            .then(cred => {
+                document.getElementById('id01').style.display = 'none';
+            })
+            .catch(err => {
+                document.getElementById('signin-status').innerText = err.code;
+            })
+    }
+}
+
+// ##############################################################
+if(signup) {
+    signup.onsubmit = e => {
+        e.preventDefault()
+        document.getElementById('signup-status').innerText = "";
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(cred => {
+                document.getElementById('id01').style.display = 'none';
+            })
+            .catch(err => {
+                document.getElementById('signup-status').innerText = err.code;
+            })
+    }
+}
+
+// ################################################################
+onAuthStateChanged(auth, user => {
+    if(user){
+        console.log('====================================');
+        console.log(user.uid);
+        console.log('====================================');
+    }
+    else{
+        console.log('====================================');
+        console.log('user not found');
+        console.log('====================================');
+    }
+})
+
+// #####################################################################
+const signout_btn = document.getElementById('signout')
+
+if(signout_btn){
+    signOut(auth)
+    console.log("Log out");
+}
